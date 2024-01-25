9205f91600216afc04c86e3bae67bffa3214e3e2
sergeavila7 <serge_avila7@yahoo.com>
2024-01-24 22:24:26 -0600

    chore: add envs

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import user from './routes/user.js';
+import db from './config/db.js';
 
 const app = express();
 
-app.use('/', user);
+try {
+  await db.authenticate();
+  console.log('Conexion correcta a la BD');
+} catch (error) {
+  console.log(error);
+}
+
+app.use('/auth', user);
 
 const port = 4000;
 app.listen(port, () => {
